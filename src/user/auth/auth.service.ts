9e3ddf224795e95d3c975d2fbec8a3ee37f4bd17
Titus <dev@tituscoleman.com>
2023-08-19 14:45:07 -0400

    add signup logic

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,5 +1,8 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { UserType } from '@prisma/client';
+import * as jwt from "jsonwebtoken"
 
 
 
@@ -17,7 +20,7 @@ export class AuthService {
         private readonly prismaService: PrismaService
     ){}
 
-    async signup({email}: SignupParams) {
+    async signup({email, password, name, phone}: SignupParams) {
         const userExists = await this.prismaService.user.findUnique({
             where: {
                 email
@@ -27,6 +30,30 @@ export class AuthService {
        if(userExists) {
         throw new ConflictException('This email exists')
        }
+
+       const hashPw = await bcrypt.hash(password, 10)
+    //    const isMatch = await bcrypt.compare(password, hashPw);
+
+    //    if(!isMatch) {
+    //     throw new UnauthorizedException
+    //    }
+    const user = await this.prismaService.user.create({
+        data: {
+            email,
+            name,
+            phone,
+            password: hashPw,
+            user_type: UserType.BUYER
+        }
+    })
+    const token = await jwt.sign(
+        {name, id: user.id}, 
+        process.env.JSON_TOKEN_KEY,{
+        expiresIn: 360000,
+    })
+
+    return token
+
     }
 
 }
